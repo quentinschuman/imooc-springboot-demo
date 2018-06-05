283fa4b6c428822b349690a70e2f41958ca67b29
quent <quentinschuman@163.com>
2018-06-05 23:40:12 +0800

    [add]thymeleaf模板标签

--- src/main/java/com/example/jike/web/ThymeleafController.java
+++ src/main/java/com/example/jike/web/ThymeleafController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * ProjectName: jike
@@ -27,6 +30,40 @@ public class ThymeleafController {
     @RequestMapping("center")
     public String center(){
         return "/freemarker/thymeleaf/center";//此处与视频中不一样 freemarker/thymeleaf/center
+    }
+
+    @RequestMapping("test")
+    public String test(ModelMap map){
+        User user = new User();
+        user.setId(10001);
+        user.setUsername("admin");
+        user.setPassword("123456");
+        map.addAttribute("user",user);
+
+        User user1 = new User();
+        user1.setId(9999);
+        user1.setUsername("test");
+        user1.setPassword("123456");
+        map.addAttribute("user",user);
+
+        User user2 = new User();
+        user1.setId(10000);
+        user1.setUsername("imooc");
+        user1.setPassword("123456");
+        map.addAttribute("user",user);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user1);
+        userList.add(user2);
+
+        return "/freemarker/thymeleaf/test";
+    }
+
+    @RequestMapping("postform")
+    public String postForm(User user){
+        System.out.println(user.getUsername());
+        return "redirect:/th/test";
     }
 
 }
